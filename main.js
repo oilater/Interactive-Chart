07d498ce10f5c2ea7b985bf70910982146842be2
oilater <oilater@naver.com>
2025-05-07 14:56:45 +0900

    Implement remove graph functions and Refactor

--- main.js
+++ main.js
@@ -6,6 +6,7 @@ const addValueButton = document.querySelector(".add-value-button");
 const applyEditButton = document.querySelector(".apply-advanced-value-button");
 
 const graphBackgroundHeight = 20;
+const graphAnimationDuration = 600;
 
 addValueButton.addEventListener('click', clickAddValueHandler);
 applyEditButton.addEventListener('click', clickApplyEditHandler);
@@ -13,78 +14,76 @@ applyEditButton.addEventListener('click', clickApplyEditHandler);
 // 전체 Value를 관리할 객체
 let wholeData = {};
 
-class UserData {
+class Data {
     constructor(id, value) {
         this.id = id;
         this.value = value;
         this.key = crypto.randomUUID();
     }
 
-    createCard() {
-        const template = document.querySelector('#card-template');
-        const clone = template.content.cloneNode(true);
-    
-        const card = clone.querySelector('.user-data');
-        card.querySelector('.user-id').textContent = this.id;
-        card.querySelector('.user-value').textContent = this.value;
-    
+    createUI() {
+        // 만들어 둔 HTML 템플릿을 복사해 카드 생성
+        const clonedCard = document.querySelector('.card-template').content.cloneNode(true);
+        const card = clonedCard.querySelector('.card-wrapper');
+        card.querySelector('.card-id').textContent = this.id;
+        card.querySelector('.card-value').textContent = this.value;
         const deleteButton = card.querySelector('.delete-button');
-        deleteButton.addEventListener("click", () => this.delete(card));
-    
-        valueTable.appendChild(card);
-    }
-
-    createGraph() {
-        const template = document.querySelector('#graph-template');
-        const clone = template.content.cloneNode(true);
-    
-        const graphWrapper = clone.querySelector('.graph-wrapper');
-        const graph = graphWrapper.querySelector('.graph');
+        
+        // 그래프 생성
+        const graphTemplate = document.querySelector('.graph-template').content.cloneNode(true);
+        const graphWrapper = graphTemplate.querySelector('.graph-wrapper');
         const graphValue = graphWrapper.querySelector('.graph-value');
+        const graphBar = graphWrapper.querySelector('.graph');
         const graphId = graphWrapper.querySelector('.graph-id');
-    
         graphId.textContent = this.id;
-    
+        
+        // 삭제 시 이벤트 설정
+        deleteButton.addEventListener("click", () => this.delete(card, graphWrapper));
+        
+        // 생성한 카드, 그래프를 UI에 추가
+        valueTable.appendChild(card);
         graphTable.appendChild(graphWrapper);
-        animateGraph(graph, this.value, graphValue);
+        animateGraph(graphBar, this.value, graphValue);
     }
 
-    // 값 삭제 시 테이블 및 wholeData에서 삭제하는 메소드
-    delete(card) {
-        card.remove();
-        if (wholeData == null || !wholeData[this.key]) {
+    delete(card, graph) {
+        if (!wholeData[this.key]) {    
             return;
         }
         delete wholeData[this.key];
-        updateAdvancedEditTextarea();
+        card.remove();
+        graph.remove();
+        updateEditTextarea();
     }
 }
 
+// 그래프 애니메이션 실행 함수
 function animateGraph(graph, targetValue, currentValue) {
     let height = 0;
-    const animationDuration = 600;
     const startTime = performance.now();
+    const interactiveColor = targetValue >= 100 ? '#1873cc' : targetValue >= 50 ? 'dodgerblue' : '#1e90ff80';
 
     function animate(time) {
-        const progress = (time - startTime) / animationDuration;
+        const progress = (time - startTime) / graphAnimationDuration;
         height = Math.min(progress * targetValue, targetValue);
 
         // 숫자 애니메이션 및 색상 설정
-        currentValue.innerHTML = `<p class="graph-value">${height <= targetValue ? Math.floor(height) : targetValue}</p>`;
-        currentValue.style.setProperty('--graph-value-color', `${targetValue >= 100 ? '#1873cc' : targetValue >= 50 ? 'dodgerblue' : '#1e90ff80'}`);
+        currentValue.textContent = height <= targetValue ? Math.floor(height) : targetValue;
+        currentValue.style.setProperty('--graph-value-color', interactiveColor);
         
         // 그래프 높이 애니메이션 및 색상 설정
         graph.style.setProperty('--graph-height', `${height / 100 * graphBackgroundHeight}rem`);
-        graph.style.setProperty('--graph-color', `${targetValue >= 100 ? '#1873cc' : targetValue >= 50 ? 'dodgerblue' : '#1e90ff80'}`);
+        graph.style.setProperty('--graph-color', interactiveColor);
 
         if (progress < 1) {
             requestAnimationFrame(animate);
         }
     }
-    
+
     requestAnimationFrame(animate);
 }
-// 값 추가 클릭 이벤트
+
+// 값 추가 클릭 시 실행되는 함수
 function clickAddValueHandler () {
     const idInput = document.querySelector('#id-input').value;
     const valueInput = document.querySelector('#value-input').value;
@@ -92,9 +91,11 @@ function clickAddValueHandler () {
     // id, value의 유효성 검사 후 반환
     const [id, value] = getValidateInput(idInput, valueInput);
     
-    const newData = new UserData(id, value);
-    appendValue(newData);
-    updateAdvancedEditTextarea();
+    if (!id || !value) {
+        return;
+    }
+    const newData = new Data(id, value);
+    applyNewData(newData);
 
     idInput.value = "";
     valueInput.value = "";
@@ -103,20 +104,20 @@ function clickAddValueHandler () {
 function getValidateInput(id, value) {
     if (!id.trim()) {
         alert("ID를 입력해주세요!");
-        return;
+        return [null, null];
     }
     else if (!value.trim()) {
         alert("VALUE를 입력해주세요!");
-        return;
+        return [null, null];
     }
 
     return [id, value];
 }
 
-function appendValue(newData) {
-    wholeData[newData.key] = newData;
-    newData.createCard();
-    newData.createGraph();
+function applyNewData(data) {
+    data.createUI();
+    wholeData[data.key] = data;
+    updateEditTextarea();
 }
 
 // 값 고급 편집 클릭 이벤트
@@ -145,13 +146,12 @@ function clickApplyEditHandler() {
             alert("비어있는 ID 값이 있는지 확인하세요!")
             return;
         }
-        const newData = new UserData(id.trim(), value);
-        appendValue(newData);
+        const newData = new Data(id.trim(), value);
+        applyNewData(newData);
     });
-    updateAdvancedEditTextarea();
 }
 
-function updateAdvancedEditTextarea() {
+function updateEditTextarea() {
     const dataList = Object.values(wholeData).map(user => ({
         id: user.id,
         value: Number(user.value),
