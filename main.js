be44d6eb86f3a64189d66a6f75953c8a2862db13
oilater <oilater@naver.com>
2025-05-09 21:19:26 +0900

    Seperate rendering roles of card and graph

--- main.js
+++ main.js
@@ -22,9 +22,9 @@ const addSection = document.querySelector(".add-value-section");
 const editAdvanceSection = document.querySelector(".edit-advanced-value-section");
 const cardTable = document.querySelector(".value-card-table");
 const graphTable = document.querySelector(".graph-table");
-const editTextarea = document.querySelector(".advanced-value-textarea");
+const jsonTextarea = document.querySelector(".advanced-value-textarea");
 const addValueButton = document.querySelector(".add-value-button");
-const applyEditButton = document.querySelector(".apply-advanced-value-button");
+const applyAdvancedValueButton = document.querySelector(".apply-advanced-value-button");
 const inputFeedBackText = document.querySelector(".feedback-text");
 const jsonFeedbackText = document.querySelector(".json-feedback-text");
 const idInput = document.querySelector("#id-input");
@@ -80,26 +80,29 @@ const renderUI = (status = RenderStatus.ALL, data = null) => {
             selectedCard.remove();
             break;
     }
-    updateEditTextarea();
+    updateJSONTextarea();
     updateSectionVisibility();
-    displayGraphButton();
+    displayGraphAddButton();
     window.scrollTo({ top: 0, behavior: 'smooth' }); // 스크롤 맨 위로 이동
 };
 
 const renderGraph = (data) => {
     // 만들어 둔 HTML 템플릿을 복사해 그래프 생성
     const template = document.querySelector('.graph-template').content.cloneNode(true);
-    const wrapper = template.querySelector('.graph-wrapper');
-    const valueText = wrapper.querySelector('.graph-value');
-    const barElement = wrapper.querySelector('.graph');
+    const graph = template.querySelector('.graph-wrapper');
 
-    // 편집 및 삭제 시 참조하기 위한 data-id 적용
-    wrapper.dataset.id = data.key;
-    wrapper.querySelector('.graph-id').textContent = data.id;
-    
-    animateGraph(data.value, barElement, valueText);
-    graphTable.appendChild(wrapper);
+    initGraph(data, graph);
+    graphTable.appendChild(graph);
 };
+
+const initGraph = (data, graph) => {
+    const graphId = graph.querySelector('.graph-id');
+    const graphValue = graph.querySelector('.graph-value');
+    const barElement = graph.querySelector('.graph');
+    graph.dataset.id = data.key; // 편집 및 삭제 시 참조하기 위한 data-id 적용
+    graphId.textContent = data.id;
+    animateGraph(data.value, barElement, graphValue);
+}
 
 const animateGraph = (targetValue, barElement, valueText) => {
     const color = getColor(targetValue);
@@ -130,50 +133,46 @@ const updateGraph = (data, graph) => {
 };
 
 const renderCard = (data) => {
-    // 템플릿을 복사해 카드 생성
     const template = document.querySelector('.card-template').content.cloneNode(true);
     const card = template.querySelector('.card-wrapper');
+
+    initCard(data, card);
+    cardTable.appendChild(card);
+};
+
+// 생성한 카드 초기화
+const initCard = (data, card) => {
     const cardId = card.querySelector('.card-id');
     const cardValue = card.querySelector('.card-value');
     const editButton = card.querySelector('.card-edit-button');
     const deleteButton = card.querySelector('.card-delete-button');
 
-    // 값 편집 및 삭제 시 카드를 참조하기 위한 data-id 적용
-    card.dataset.id = data.key;
-
+    card.dataset.id = data.key; // 값 편집 및 삭제 시 카드를 참조하기 위한 data-id 적용
     cardId.textContent = data.id;
     cardValue.textContent = data.value;
-
-    const color = getColor(data.value);
+    const color = getColor(data.value); // value에 따라 색상을 지정
     cardValue.style.color = color;
     card.style.borderColor = color;
 
-    editButton.addEventListener("click", () => handleEditCard(card, data));
-    deleteButton.addEventListener("click", () => {
-        clearFields();
-        card.classList.add("fade-out");
-        // fadeOut 애니메이션 종료 후 카드를 삭제
-        card.addEventListener("animationend", () => {
-            delete wholeData[data.key];
-            renderUI(RenderStatus.DELETE, data);
-        });
+    deleteButton.addEventListener("click", (e) => onDeleteData(e, data, card));
+    editButton.addEventListener("click", (e) => onEditData(e, data, card));
+}
+
+const onDeleteData = (e, data, card) => {
+    e.preventDefault();
+    clearFields();
+    card.classList.add("fade-out");
+    // fadeOut 애니메이션 종료 후 카드를 삭제
+    card.addEventListener("animationend", () => {
+        delete wholeData[data.key];
+        renderUI(RenderStatus.DELETE, data);
     });
-    
-    cardTable.appendChild(card);
-};
-
-// 편집한 값에 해당하는 카드 업데이트
-const updateCard = (data, card) => {
-    const cardValue = card.querySelector('.card-value');
-    cardValue.textContent = data.value;
-    const color = getColor(data.value);
-    cardValue.style.color = color;
-    card.style.borderColor = color;
-};
+}
 
 // 카드의 수정 버튼 클릭 시
-const handleEditCard = (card, data) => {
-    // 카드의 value는 편집 모드에서는 p 태그에서 input으로 변함
+const onEditData = (e, data, card) => {
+    e.preventDefault();
+    // value element는 편집 모드에서는 input 태그, 그 외에는 p 태그로 변함
     let valueElement = card.querySelector('.card-value');
 
     // 편집 모드 진입
@@ -184,6 +183,7 @@ const handleEditCard = (card, data) => {
         inputElement.classList.add('card-value');
         inputElement.value = valueElement.textContent;
         inputElement.setAttribute('maxlength', `${MAX_VALUE_LENGTH}`);
+        // valueElement의 참조 변경
         valueElement.replaceWith(inputElement);
         valueElement = inputElement;
         valueElement.focus();
@@ -201,6 +201,7 @@ const handleEditCard = (card, data) => {
         const pElement = document.createElement('p');
         pElement.classList.add('card-value');
         pElement.textContent = valueElement.value;
+        // valueElement의 참조 변경
         valueElement.replaceWith(pElement);
         valueElement = pElement;
 
@@ -237,15 +238,23 @@ const handleEditCard = (card, data) => {
     enterEditMode(); // 편집 모드 진입
 };
 
-// 값에 따라 색상을 결정
 const getColor = (value) => {
     return value >= 100 ? COLOR_HIGH_VALUE : value >= 50 ? COLOR_MEDIUM_VALUE : COLOR_LOW_VALUE;
 };
 
+// 편집한 값에 해당하는 카드 업데이트
+const updateCard = (data, card) => {
+    const cardValue = card.querySelector('.card-value');
+    cardValue.textContent = data.value;
+    const color = getColor(data.value);
+    cardValue.style.color = color;
+    card.style.borderColor = color;
+};
+
 // Textarea에 JSON 반영
-const updateEditTextarea = () => {
+const updateJSONTextarea = () => {
     const dataList = Object.values(wholeData).map((data) => data.toJSON());
-    editTextarea.value = dataList.length !=0 ? JSON.stringify(dataList, null, 2) : '';
+    jsonTextarea.value = dataList.length !=0 ? JSON.stringify(dataList, null, 2) : '';
 };
 
 // 데이터 수에 따라 각 Section 표시 여부 결정
@@ -274,6 +283,19 @@ const updateSectionVisibility = () => {
     editSection.style.display = sectionVisibility;
 };
 
+// 가장 끝에 있는 그래프의 오른쪽에 + 버튼을 추가
+const displayGraphAddButton = () => {
+    const graphs = document.querySelectorAll('.graph-wrapper');
+    if (graphs.length === 0) return;
+    const lastButton = graphs[graphs.length - 1].querySelector('.graph-add-button');
+    if (!lastButton) return;
+    
+    lastButton.addEventListener('click', () => {
+        window.scrollTo({ top: document.documentElement.scrollHeight, behavior: 'smooth' });
+        idInput.focus({preventScroll: true});
+    });
+}
+
 // 초기화
 const clearFields = () => {
     idInput.value = '';
@@ -281,12 +303,12 @@ const clearFields = () => {
     inputFeedBackText.textContent = '';
     jsonFeedbackText.textContent = '';
     addValueButton.classList.remove(ACTIVE_COLOR);
-    applyEditButton.classList.remove(ACTIVE_COLOR);
+    applyAdvancedValueButton.classList.remove(ACTIVE_COLOR);
 }
 
 const setEventListeners = () => {
-    // 입력 여부에 따라 Add 버튼 색 변경
-    const handleAddButtonColor = (e) => {
+    // Input의 입력 이벤트를 받아 체크 후 feedback 텍스트, 버튼 색 결정
+    const onCheckInput = (e) => {
         e.preventDefault();
         const id = idInput.value.trim();
         const value = valueInput.value.trim();
@@ -309,35 +331,19 @@ const setEventListeners = () => {
         inputFeedBackText.textContent = feedback;
     };
 
-    // 입력 여부에 따라 Apply 버튼 색 변경
-    const handleApplyButtonColor = (e) => {
+    // 고급 값 편집 Textarea의 입력 이벤트를 받아 체크 후 버튼 색 결정
+    const onCheckTextarea = (e) => {
         e.preventDefault();
-        const text = editTextarea.value;
+        const text = jsonTextarea.value;
 
         if (text.trim()) {
-            applyEditButton.classList.add(ACTIVE_COLOR);
+            applyAdvancedValueButton.classList.add(ACTIVE_COLOR);
         } else {
-            applyEditButton.classList.remove(ACTIVE_COLOR);
+            applyAdvancedValueButton.classList.remove(ACTIVE_COLOR);
         }
     };
 
-    // input 값 검사
-    const isInputValidate = (id, value) => {
-        if (!id || !id.trim()) {
-            return false;
-        } else if (!value.trim() || isNaN(value)) {
-            return false;
-        }
-        return true;
-    }
-
-    // input 이벤트 설정
-    idInput.addEventListener('input', handleAddButtonColor);
-    valueInput.addEventListener('input', handleAddButtonColor);
-    editTextarea.addEventListener('input', handleApplyButtonColor);
-
-    // 값 입력 후 Add 버튼 클릭 시
-    addValueButton.addEventListener('click', (e) => {
+    const onAddValue = (e) => {
         e.preventDefault();
         const id = idInput.value;
         const value = valueInput.value;
@@ -348,14 +354,13 @@ const setEventListeners = () => {
         
         renderUI(RenderStatus.ADD, newData);
         clearFields();
-    });
+    };
 
-    // JSON 편집 후 Apply 버튼 클릭 시
-    applyEditButton.addEventListener('click', (e) => {
+    const onApplyAdvancedValue = () => {
         e.preventDefault();
         try {
             // JSON 파싱
-            const parsed = JSON.parse(editTextarea.value);
+            const parsed = JSON.parse(jsonTextarea.value);
             
             // 유효성 검사
             if (!Array.isArray(parsed) || parsed.length == 0) {
@@ -380,21 +385,23 @@ const setEventListeners = () => {
         } catch (error) {
             jsonFeedbackText.textContent = "* 올바른 형식의 JSON을 입력해주세요."
         }
-    });
-};
+    }
 
-// 가장 끝에 있는 그래프의 오른쪽에 + 버튼을 추가
-const displayGraphButton = () => {
-    const graphs = document.querySelectorAll('.graph-wrapper');
-    if (graphs.length === 0) return;
-    const lastButton = graphs[graphs.length - 1].querySelector('.graph-add-button');
-    if (!lastButton) return;
-    
-    lastButton.addEventListener('click', () => {
-        window.scrollTo({ top: document.documentElement.scrollHeight, behavior: 'smooth' });
-        idInput.focus({preventScroll: true});
-    });
-}
+    const isInputValidate = (id, value) => {
+        if (!id || !id.trim()) {
+            return false;
+        } else if (!value.trim() || isNaN(value)) {
+            return false;
+        }
+        return true;
+    };
+
+    idInput.addEventListener('input', onCheckInput);
+    valueInput.addEventListener('input', onCheckInput);
+    jsonTextarea.addEventListener('input', onCheckTextarea);
+    addValueButton.addEventListener('click', onAddValue);
+    applyAdvancedValueButton.addEventListener('click', onApplyAdvancedValue);
+};
 
 setEventListeners();
 // 초기 렌더링
